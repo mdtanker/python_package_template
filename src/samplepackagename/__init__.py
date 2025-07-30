d9b3d2fd8526082e908ab90c12ce2dfd6fdbe9d4
Matt Tankersley <81199856+mdtanker@users.noreply.github.com>
2025-07-30 13:43:11 +0200

    remove logging basicConfig and NullHandler (#15)

--- src/samplepackagename/__init__.py
+++ src/samplepackagename/__init__.py
@@ -8,11 +8,7 @@ from ._version import version as __version__
 __all__ = ["__version__"]
 
 
-logging.basicConfig(level=logging.INFO)
-
 logger = logging.getLogger(__name__)
-
-logger.addHandler(logging.NullHandler())
 
 from .module1 import (
     # List of functions and classes to be imported when using `import samplepackagename`
